36fd93059ba9614b6c7761dda405c6821900b365
aaliyahsykes04 <ats136@pitt.edu>
2026-01-20 20:23:59 -0500

    C# Day 1

---
+++ Aaliyah's Bank/Class Library/CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using Class_Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Class_Library.Account;
+
+namespace Class_Library
+{
+    public class CheckingAccount : Account
+    {
+        public decimal OverdraftLimit { get; private set; }
+
+        public CheckingAccount(int accountNumber, string ownerName, decimal overdraftLimit)
+           : base(accountNumber, ownerName, AccountType.Checking)
+        {
+            OverdraftLimit = overdraftLimit;
+        }
+
+        public override bool Withdraw(decimal amount)
+        {
+            if (amount <= 0m) return false;
+
+            // Allow overdraft up to the limit
+            if (Balance + OverdraftLimit < amount) return false;
+
+            Balance -= amount;
+            return true;
+        }
+    }
+
+}
---
+++ Aaliyah's Bank/ConsoleTestingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Class_Library;
+
+Console.WriteLine("Console Testing App for Liyahs Bank Library");
+
+//Base Account Test
+
+Account testAcct = new Account();
+testAcct.IsActive = true;
+Console.WriteLine("Base Account Created:");
+Console.WriteLine($"Account Number: {testAcct.AccountNumber}");
+Console.WriteLine($"Owner Name: {testAcct.OwnerName}");
+Console.WriteLine($"Balance : {testAcct.Balance: C}");
+Console.WriteLine($"Is Actvie: {testAcct.IsActive}");
+Console.WriteLine($"Date Opened: {testAcct.DateOpened}");
+
+//Base Account Deposit Test
+testAcct.Deposit(500);
+Console.WriteLine($"After depositing $500, Balance: {testAcct.Balance:C}");
+
+//Base Account Withdraw Test
+bool withdrawSuccess = testAcct.Withdraw(200);
+Console.WriteLine($"Attempt to withdrae $200: {(withdrawSuccess ? "Success" : "Failed")}, New Balance: {testAcct.Balance:C}");
